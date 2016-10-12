890473dbcd97bde657d7b08af386b470d01a4e3f
LeeWongSnail <@wangli_0632@163.com>
2016-10-12 14:33:36 +0800

    implement

--- ALiVideoRecorder/ALiVideoRecorder/ALiVideoRecorder/ALiAssetWriter.h
+++ ALiVideoRecorder/ALiVideoRecorder/ALiVideoRecorder/ALiAssetWriter.h
@@ -7,7 +7,53 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreMedia/CoreMedia.h>
 
 @interface ALiAssetWriter : NSObject
 
+@property (nonatomic, readonly) NSString *path;
+
+/**
+ *  WCLRecordEncoder遍历构造器的
+ *
+ *  @param path 媒体存发路径
+ *  @param cy   视频分辨率的高
+ *  @param cx   视频分辨率的宽
+ *  @param ch   音频通道
+ *  @param rate 音频的采样比率
+ *
+ *  @return WCLRecordEncoder的实体
+ */
++ (ALiAssetWriter*)encoderForPath:(NSString*)path Height:(NSInteger)cy width:(NSInteger)cx channels: (int)ch samples:(Float64)rate;
+
+/**
+ *  初始化方法
+ *
+ *  @param path 媒体存发路径
+ *  @param cy   视频分辨率的高
+ *  @param cx   视频分辨率的宽
+ *  @param ch   音频通道
+ *  @param rate 音频的采样率
+ *
+ *  @return WCLRecordEncoder的实体
+ */
+- (instancetype)initPath:(NSString*)path Height:(NSInteger)cy width:(NSInteger)cx channels: (int)ch samples:(Float64)rate;
+
+/**
+ *  完成视频录制时调用
+ *
+ *  @param handler 完成的回掉block
+ */
+- (void)finishWithCompletionHandler:(void (^)(void))handler;
+
+/**
+ *  通过这个方法写入数据
+ *
+ *  @param sampleBuffer 写入的数据
+ *  @param isVideo      是否写入的是视频
+ *
+ *  @return 写入是否成功
+ */
+- (BOOL)encodeFrame:(CMSampleBufferRef)sampleBuffer isVideo:(BOOL)isVideo;
 @end
--- ALiVideoRecorder/ALiVideoRecorder/ALiVideoRecorder/ALiVideoRecorder.h
+++ ALiVideoRecorder/ALiVideoRecorder/ALiVideoRecorder/ALiVideoRecorder.h
@@ -6,8 +6,47 @@
 //  Copyright © 2016年 LeeWong. All rights reserved.
 //
 
+#import <AVFoundation/AVCaptureVideoPreviewLayer.h>
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol ALiVideoRecordDelegate <NSObject>
+
+- (void)recordProgress:(CGFloat)progress;
+
+@end
 
 @interface ALiVideoRecorder : NSObject
+
+@property (atomic, strong) NSString *videoPath;//视频路径
+
+@property (nonatomic, assign) NSInteger maxVideoDuration;   //最长视频时长
+
+@property (nonatomic, weak) id <ALiVideoRecordDelegate> delegate;
+
+//捕获到的视频呈现的layer
+- (AVCaptureVideoPreviewLayer *)previewLayer;
+//启动录制功能
+- (void)startUp;
+//关闭录制功能
+- (void)shutdown;
+//开始录制
+- (void) startCapture;
+//暂停录制
+- (void) pauseCapture;
+//停止录制
+- (void) stopCaptureHandler:(void (^)(UIImage *movieImage))handler;
+//继续录制
+- (void) resumeCapture;
+//开启闪光灯
+- (void)openFlashLight;
+//关闭闪光灯
+- (void)closeFlashLight;
+//切换前后置摄像头
+- (void)changeCameraInputDeviceisFront:(BOOL)isFront;
+//将mov的视频转成mp4
+- (void)changeMovToMp4:(NSURL *)mediaURL dataBlock:(void (^)(UIImage *movieImage))handler;
+
+
 
 @end
